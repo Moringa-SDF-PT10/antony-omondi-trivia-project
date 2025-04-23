91702648b60ed0f1a0b61bf03e8ed272ac72554f
Antony Omondi <tonymboya13@gmail.com>
2025-04-23 22:48:36 +0300

    added function to retrieve the questions

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const start = document.getElementById("start-button")
+    const quizSection = document.getElementById("quiz-section")
+    const questionContent = document.getElementById("question-content")
+    const ansButtons = document.getElementById("answer-btns")
+    const next = document.getElementById("next-button")
+    const resultsDisplay = document.getElementById("result-section")
+    const finalScore = document.getElementById("final-score")
+    const resetQuiz = document.getElementById("reset-btn")
+
+    let questionIndex = 0
+    let score = 0
+    let questions = []
+
+    function startTrivia(){
+        fetch("https://opentdb.com/api.php?amount=5&type=multiple")
+        .then(res => res.json())
+        .then(data => {
+            questions = data.results
+            start.classList.add("hidden")
+            quizSection.classList.remove("hidden")
+            
+        })
+    }
+})
